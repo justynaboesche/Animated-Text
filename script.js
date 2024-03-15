6a29c739289df6f099d589b5618eb3f0e67704c5
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-15 13:38:39 +0100

    function writingAnimation created

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Nic tu nie ma 😱')
+const modal = document.querySelector('.modal');
+const input = document.querySelector('.modal-input');
+const modalBtn = document.querySelector('.modal-btn');
+const saveBtn = document.querySelector('.save-btn');
+const text = document.querySelector('.text');
+const errorMsg = document.querySelector('.error-msg');
+
+let inputValue = 'To jest testowa wiadomość';
+let timeout;
+let index = 1;
+let speed = 80;
+
+const writingAnimation = () => {
+	text.innerHTML = inputValue.slice(0, index);
+	index++;
+
+    if(index > inputValue.length) return
+
+    timeout = setTimeout(writingAnimation, speed)
+};
+
+writingAnimation()
